6675429bcb8b0aac3985b85efaab3e6f3c0bd9e0
Jimmy Oliger <hi@jimmyoliger.com>
2015-03-31 11:03:01 +0200

    no more liquid

--- .gulp/tasks/markup.js
+++ .gulp/tasks/markup.js
@@ -1,20 +1,6 @@
-var gulp        = require('gulp');
-var liquid      = require('gulp-liquid');
-var rename      = require('gulp-rename');
-var config      = require('../config').markup
-var Yaml        = require('yamljs');
-
-var env = false;
-try {
-  env = Yaml.load('env.yml');
-} catch(e) {}
-
+var gulp = require('gulp');
+var config = require('../config').markup
 
 gulp.task('markup', function() {
-  gulp.src(config.src)
-    .pipe(rename({ suffix: ".liquid" }))
-    .pipe(gulp.dest(config.dest));
-  return env && gulp.src(config.src)
-    .pipe(liquid({ locals: env }))
-    .pipe(gulp.dest(config.dest));
+  return gulp.src(config.src).pipe(gulp.dest(config.dest));
 });
