bf946d3b5a2d3a70fc5e10ac3d40e6a8b5163208
Stephane Bellity <sbellity@gmail.com>
2015-05-19 16:28:34 +0200

    Image sizes settings

--- .gulp/tasks/markup.js
+++ .gulp/tasks/markup.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var config = require('../config').markup;
-var _ = require('underscore');
 var inlineStyle = require('gulp-inline-style');
 
 gulp.task('markup', ['sass'], function() {
